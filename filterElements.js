548818450088c1dcbcc7b511907f2cd0368a1e8c
Yogesh <yogeshmind222@gmail.com>
2024-03-13 20:07:30 +0530

    Problem filter the elements from array

---
+++ filterElements.js
@@ -0,0 +1,9 @@
+var filter = function(arr, fn) {
+    let output = [];
+    for (let i=0; i<arr.length; i++){
+        if(fn(arr[i], i)){
+            output.push(arr[i])
+        }
+    }
+    return output
+};
