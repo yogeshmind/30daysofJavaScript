be6d5b5d9a914c3ef81db11b2308dec70950dcd0
Yogesh <yogeshmind222@gmail.com>
2024-03-23 08:29:18 +0530

    Time Limit problem of javascript on leetcode

---
+++ timeLimit.js
@@ -0,0 +1,19 @@
+var timeLimit = function (fn, t) {
+    return async function (...args) {
+        return new Promise((resolve, reject) => {
+            const timer = setTimeout(() => {
+                reject("Time Limit Exceeded")
+            }, t)
+
+            fn(...args)
+                .then((result) => {
+                    clearTimeout(timer)
+                    resolve(result)
+                })
+                .catch((err) => {
+                    clearTimeout(timer)
+                    reject(err)
+                })
+        })
+    }
+}
