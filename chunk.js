3df9c7f70fa489db4aa1f3468a621f7f90dd70a2
Yogesh <yogeshmind222@gmail.com>
2024-03-28 21:15:11 +0530

    problem of 28-03-2024

---
+++ chunk.js
@@ -0,0 +1,11 @@
+var chunk = function(arr, size) {
+    const chunked = []
+    let index = 0
+
+    while (index < arr.length) {
+        chunked.push(arr.slice(index, size + index))
+        index += size
+    }
+    
+    return chunked
+};
