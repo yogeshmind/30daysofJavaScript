67ce86984ce8e7d824ea7bf6fcdf1dd2f2a4d85e
Yogesh <yogeshmind222@gmail.com>
2024-03-30 08:22:19 +0530

    Problem of 30-03-2024

---
+++ promiseAll.js
@@ -0,0 +1,25 @@
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => {
+        let results = []
+        let counter = 0
+
+        if (functions.length === 0) {
+            resolve(results)
+        }
+
+        for (let i = 0; i < functions.length; i++) {
+            functions[i]()
+                .then(result => {
+                    results[i] = result
+                    counter++
+
+                    if (counter === functions.length) {
+                        resolve(results)
+                    }
+                })
+                .catch(reason => {
+                    reject(reason)
+                })
+        }
+    })
+};
